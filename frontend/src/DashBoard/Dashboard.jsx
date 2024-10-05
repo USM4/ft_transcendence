74b1e15bf768ff84ab101b708e6c9b92aa1309bc
USM4 <oussamaredoine06@gmail.com>
2024-10-05 08:47:36 +0100

    added a navbar for the dashboard now implementing the styling of the buttons

--- frontend/src/DashBoard/Dashboard.jsx
+++ frontend/src/DashBoard/Dashboard.jsx
@@ -8,6 +8,13 @@ function Dashboard() {
           <div>
               <SideBar/>
           </div>
+          <div className="dash-navbar">
+            <ul >
+              <li><button className="nav-btns">notification</button></li>
+              <li><button className="nav-btns">search</button></li>
+              <li><button className="nav-btns">profile</button></li>
+            </ul>
+          </div> 
          <div className="main-dashbord-content">
            <h1>Welcome to the Dashboard</h1>
            <p>This is your main content area.</p>
