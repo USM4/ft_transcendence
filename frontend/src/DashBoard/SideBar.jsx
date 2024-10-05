1e86b0dc96cdfbd95dc0247b444499aaa11072f4
USM4 <oussamaredoine06@gmail.com>
2024-10-05 02:49:04 +0100

    added the dashboard sidebar still building the dashboard

--- frontend/src/DashBoard/SideBar.jsx
+++ frontend/src/DashBoard/SideBar.jsx
@@ -1,48 +1,66 @@
 import React from "react";
-import './Dashboard.css'
+import '../App.css'
 import HomeIcon from '@mui/icons-material/Home'
 import SendIcon from '@mui/icons-material/Send';
 import GamepadIcon from '@mui/icons-material/Gamepad';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
+import ReorderIcon from '@mui/icons-material/Reorder';
 function SideBar() {
     const SideBarData = [
         {
+            title: "List",
+            icon: <ReorderIcon/>,
+            link: '/dashboard'
+        },
+        {
             title: "Home",
             icon: <HomeIcon/>,
-            link: "/home"
+            link: "/dashboard"
         },
         {
             title: "Chat",
             icon: <SendIcon/>,
-            link: "/chat"
+            // chat scoope
+            link: "/dashboard"
         },
         {
             title: "Game",
             icon: <GamepadIcon/>,
-            link: "/game"
+            // Game scoope
+            link: "/dashboard"
         },
         {
             title: "Friends",
             icon: <PeopleAltIcon/>,
-            link: "/friends"
+            link: "/dashboard"
         },
+        {
+            title: "Logout",
+            icon: <KeyboardReturnIcon/>,
+            link: "/"
+        },
+
     ]
+    const first_icon = SideBarData[0];
+    const last_icon = SideBarData[SideBarData.length - 1];
+    const middle_icons = SideBarData.slice(1, SideBarData.length - 1)
     return (
         <div className="SideBar">
-            <p>hahhahahahhahahahaha</p>
             <ul>
-                {SideBarData.map((key, value) => {
+                <li className="first-sidebar-icon" onClick={() => {window.location.pathname = first_icon.link}}>
+                    <div> {first_icon.icon}</div>
+                </li>
+                {middle_icons.map((value, key) => {
                 return(
-                    <li key={key} onClick={() => {window.location.pathname = value.link}}> 
-                    {" "}
-                    <div>{value.icon}</div> {" "}
-                    <div> 
-                        {value.title}
-                    </div>
+                    <li  className="middle-sidebar-icon" key={key} onClick={() => {window.location.pathname = value.link}}>
+                        <div>{value.icon}</div>
                     </li>
                 );
                 })}
+                <li className="last-sidebar-icon" onClick={() => {window.location.pathname = last_icon.link}}>
+                    <div> {last_icon.icon}</div>
+                </li>
             </ul>
         </div>
 
