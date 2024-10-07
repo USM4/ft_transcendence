1eeaca2afc00ddfb6b77b55e467e2baa79e04eb4
USM4 <oussamaredoine06@gmail.com>
2024-10-07 16:39:54 +0100

    edited nav bar

--- frontend/src/DashBoard/Dashboard.jsx
+++ frontend/src/DashBoard/Dashboard.jsx
@@ -8,22 +8,13 @@ function Dashboard() {
 
     return (
       <div className="dashboard-container">
-          <div>
-              <SideBar/>
-          </div>
-          <div className="dash-navbar">
-            <ul >
-              <li><button className="nav-btns">Notifications</button></li>
-              {/* <li><div className="nav-icons"><NotificationsIcon/></div></li> */}
-              <li><button className="nav-btns">Search</button></li>
-              {/* <li><div className="nav-icons"><SearchIcon/></div></li> */}
-              <li><button className="nav-btns">Profile</button></li>
-              {/* <li><div className="nav-icons"><PermIdentityIcon/></div></li> */}
-            </ul>
-          </div> 
+          <SideBar/>
          <div className="main-dashbord-content">
+          <div>
+
            <h1>Welcome to the Dashboard</h1>
            <p>This is your main content area.</p>
+          </div>
          </div>
        </div>
     )
--- frontend/src/DashBoard/SideBar.jsx
+++ frontend/src/DashBoard/SideBar.jsx
@@ -1,55 +1,69 @@
 import React from "react";
-import '../App.css'
-import HomeIcon from '@mui/icons-material/Home'
-import SendIcon from '@mui/icons-material/Send';
-import GamepadIcon from '@mui/icons-material/Gamepad';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
-import ReorderIcon from '@mui/icons-material/Reorder';
+import "../App.css";
+import HomeIcon from "@mui/icons-material/Home";
+import SendIcon from "@mui/icons-material/Send";
+import GamepadIcon from "@mui/icons-material/Gamepad";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import ReorderIcon from "@mui/icons-material/Reorder";
 function SideBar() {
-    const SideBarData = [
-        {
-            title: "Home",
-            icon: <HomeIcon/>,
-            link: "/dashboard"
-        },
-        {
-            title: "Chat",
-            icon: <SendIcon/>,
-            // chat scoope
-            link: "/dashboard"
-        },
-        {
-            title: "Game",
-            icon: <GamepadIcon/>,
-            // Game scoope
-            link: "/dashboard"
-        },
-        {
-            title: "Friends",
-            icon: <PeopleAltIcon/>,
-            link: "/dashboard"
-        },
-        {
-            title: "Logout",
-            icon: <KeyboardReturnIcon/>,
-            link: "/"
-        },
-
-    ]
-    return (
-        <div className="SideBar">
-            <ul>
-                {SideBarData.map((value, key) => {
-                return(
-                    <li  className="middle-sidebar-icon" key={key} onClick={() => {window.location.pathname = value.link}}>
-                        <div>{value.icon}</div>
-                    </li>
-                );
-                })}
-            </ul>
+  const SideBarData = [
+    {
+      title: "Home",
+      icon: <HomeIcon />,
+      link: "/dashboard",
+    },
+    {
+      title: "Chat",
+      icon: <SendIcon />,
+      // chat scoope
+      link: "/dashboard",
+    },
+    {
+      title: "Game",
+      icon: <GamepadIcon />,
+      // Game scoope
+      link: "/dashboard",
+    },
+    {
+      title: "Friends",
+      icon: <PeopleAltIcon />,
+      link: "/dashboard",
+    },
+  ];
+  const logout = {
+    title: "Logout",
+    icon: <KeyboardReturnIcon />,
+    link: "/",
+  };
+  return (
+    <div className="SideBar">
+      <ul>
+        {SideBarData.map((value, key) => {
+          return (
+            <li
+              className={`middle-sidebar-icon-${value.title}`}
+              key={key}
+              onClick={() => {
+                window.location.pathname = value.link;
+              }}
+            >
+              <div>{value.icon}</div>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="logout-icon-container">
+        <div
+          className={`middle-sidebar-icon-${logout.title}`}
+          onClick={() => {
+            window.location.pathname = logout.link;
+          }}
+        >
+          <div className="logout-icon">{logout.icon}</div>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
 export default SideBar;
