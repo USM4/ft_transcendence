849914bf973a2bceb432021cb7b8c1e610b3307d
USM4 <oussamaredoine06@gmail.com>
2024-10-09 14:52:00 +0100

    styled the navbar for the dashboard

--- frontend/src/DashBoard/Dashboard.jsx
+++ frontend/src/DashBoard/Dashboard.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import SideBar from "./SideBar.jsx";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import SearchIcon from '@mui/icons-material/Search';
-import '../App.css'
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SearchIcon from "@mui/icons-material/Search";
+import "../App.css";
 function Dashboard() {
-
-    return (
-      <div className="dashboard-container">
-          <SideBar/>
-         <div className="main-dashbord-content">
-            <div className="search-bar-div">
-              <input className="search-bar" placeholder="Search"></input>
-              <div className="search-icon"><button className="search-btn"><SearchIcon fontSize="large"/></button></div>
-              <div className="notification-div"><button className="notification-btn"><NotificationsIcon fontSize="large"/></button></div>
-              <div className="nav-img"> <a><img className="profile-img" src="./realone.png"></img></a></div>
+  return (
+    <div className="dashboard-container">
+      <SideBar />
+      <div className="main-dashbord-content">
+        <div className="dashboard-navbar">
+          <div className="search-btn">
+            <input className="search-input" placeholder="Search" />
+            <button>
+              <SearchIcon />
+            </button>
+          </div>
+          <div className="notification-and-profile">
+            <div className="notification">
+              <button>
+                <NotificationsIcon />
+              </button>
             </div>
-            {/* <div className="dashboard-body">
-             <h1>Welcome to the Dashboard</h1>
-             <p>This is your main content area.</p>
-            </div> */}
-            {/* <Outlet/> */}
-         </div>
-       </div>
-    )
+            <div className="profile">
+              <a>Profile</a>
+            </div>
+          </div>
+        </div>
+
+        {/* <Outlet/> */}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
