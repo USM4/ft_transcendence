31559a74ea9bee454a78fc0c408695069d2be131
USM4 <oussamaredoine06@gmail.com>
2024-10-12 00:00:01 +0100

    working on dashboard friends and still not flexed properly

--- frontend/src/DashBoard/DashboardFriends.jsx
+++ frontend/src/DashBoard/DashboardFriends.jsx
@@ -7,6 +7,7 @@ function DashboardFriends() {
         <div className="dashboard-friend-item">
             <div className="friend-info">
                 <img src="player1.jpeg" alt=""/>
+                <p> Oussama Redoine </p>
             </div>
             <div className="friend-invite">
                 <button className="invite-btn"> invite </button>
