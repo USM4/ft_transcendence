32ce50868e85551b0df41a318983da24e21ac12c
USM4 <oussamaredoine06@gmail.com>
2024-10-16 22:29:30 +0100

    working on profile settings ended a side still the right side

--- frontend/src/DashBoard/ProfileSettings.jsx
+++ frontend/src/DashBoard/ProfileSettings.jsx
@@ -5,11 +5,22 @@ import GamepadIcon from "@mui/icons-material/Gamepad";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
 import ReorderIcon from "@mui/icons-material/Reorder";
+import oredoine from '/oredoine.jpeg'
 
 function ProfileSettings(){
     return(
         <div className="settings-component">
-            
+            <div className="profile-settings">
+                <div className="settings-user-image">
+                    <img src={oredoine} alt="" />
+                    <p> USM4 </p>
+                </div>
+                <div className="settings-options">
+                    <div className="general-profile-settings"><button >General settings</button></div>
+                    <div className="two-fa-settings"><button > Two Factor Settings </button></div>
+                </div>
+            </div>
+            <div className="profile-edit-settings"></div>
         </div>
     );
 };
