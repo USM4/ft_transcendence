54e340ea48140ce5d64e139606c830e4803ecab5
USM4 <oussamaredoine06@gmail.com>
2024-10-17 00:49:20 +0100

    near to complete the settings till tomorrow

--- frontend/src/DashBoard/ProfileSettings.jsx
+++ frontend/src/DashBoard/ProfileSettings.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import SendIcon from "@mui/icons-material/Send";
 import GamepadIcon from "@mui/icons-material/Gamepad";
@@ -8,6 +8,15 @@ import ReorderIcon from "@mui/icons-material/Reorder";
 import oredoine from '/oredoine.jpeg'
 
 function ProfileSettings(){
+    const [isTwoFactor,setisTwoFactor] = useState(false)
+    // const [isEnabled,setIsEnabled] = useState(false)
+    
+    const handleSwitch = () => {
+        setisTwoFactor(!isTwoFactor);
+    };
+    // const handleEnableChange = (event) => {
+    //     setIsEnabled(event.target.value === 'enable');
+    // };
     return(
         <div className="settings-component">
             <div className="profile-settings">
@@ -16,11 +25,38 @@ function ProfileSettings(){
                     <p> USM4 </p>
                 </div>
                 <div className="settings-options">
-                    <div className="general-profile-settings"><button >General settings</button></div>
-                    <div className="two-fa-settings"><button > Two Factor Settings </button></div>
+                    <div className="general-profile-settings" onClick={() => setisTwoFactor(false)}><button >General settings</button></div>
+                    <div className="two-fa-settings" onClick={() => setisTwoFactor(true)}><button > Two Factor Settings </button></div>
                 </div>
             </div>
-            <div className="profile-edit-settings"></div>
+            <div className="profile-edit-settings">
+                <div className="profile-edit-settings-title">
+                    {isTwoFactor ? "Two Factor Settings" : "General Settings"}
+                </div>
+                {isTwoFactor ? (
+                        <>
+                          
+                        </>
+                ) : (
+                    <>
+                        <div className="update-avatar">
+                            <p> Update Avatar : </p>
+                            {/* onChange={handleFileChange} */}
+                            <div className="custom-file-upload">
+                                <input type="file" accept="image/*"/>
+                            </div>
+                        </div>
+                        <div className="update-nickname"> 
+                            <p>Update Nickname : </p>
+                            <input type="text"/>
+                        </div>
+                    </>
+                )}
+                
+                <div className="save-settings">
+                    <button>Save</button>
+                </div>
+            </div>
         </div>
     );
 };
--- tools.md
+++ tools.md
@@ -1,2 +1,39 @@
 **Inspect the Database Schema**   
 >PRAGMA table_info(authentification_client);  
+
+temporary
+// <div>
+//     <p>
+//         Two Factor Authentication (2FA) adds an extra layer of security to your account. 
+//         With 2FA enabled, you'll need to provide a second form of verification, such as a code sent to your mobile device, in addition to your password.
+//     </p>
+//     <div className="two-fa-options">
+//         <label>
+//             <input 
+//                 type="radio" 
+//                 value="enable" 
+//                 checked={isEnabled} 
+//                 onChange={handleEnableChange} 
+//             />
+//             Enable 2FA
+//         </label>
+//         <label>
+//             <input 
+//                 type="radio" 
+//                 value="disable" 
+//                 checked={!isEnabled} 
+//                 onChange={handleEnableChange} 
+//             />
+//             Disable 2FA
+//         </label>
+//     </div>
+//     {isEnabled && (
+//         <div className="enable-info">
+//             <p>
+//                 To complete the setup of 2FA, please download an authentication app (like Google Authenticator) or check your SMS for codes. 
+//                 Make sure to keep your backup codes safe!
+//             </p>
+//             {/* Add fields for entering the verification code or app information */}
+//         </div>
+//     )}
+// </div>
