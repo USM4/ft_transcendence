9f0541b318474e66b5e70f68e8878fa3aa4174cd
yahiarrh <yahiarrh@gmail.com>
2024-11-17 12:09:42 +0100

    online status for side bar

--- frontend/src/Chat/Chat-sidebar.jsx
+++ frontend/src/Chat/Chat-sidebar.jsx
@@ -4,6 +4,7 @@ import Chat_area from './Chat-area.jsx';
 import Chat_input from './Chat-input.jsx';
 import '../App.css'
 import ChatIcon from '@mui/icons-material/Chat';
+import { Badge } from '@mui/material';
 
 export default function Chat_sidebar({ friends }) {
   const [selectedFriend, setSelectedFriend] = useState(null);
@@ -11,13 +12,19 @@ export default function Chat_sidebar({ friends }) {
   function handleClick(friend) {
     setSelectedFriend(friend);
   }
-  
+
   const friendsList = friends.map((friend, index) => (
     <>
       <li key={index} className="user" onClick={() => handleClick(friend)}>
         <div className="avatar">
           <img src={friend.avatar} alt={`${friend.avatar}'s avatar`} />
-
+          {/* I NEED THE ONLINE STATUS TO CHECK WICH ONE TO DISPLAY */}
+          {/* ONLINE         
+            <Badge variant="dot"
+            color="success" showZero badgeContent="" overlap="circular"></Badge>  */}
+          {/* OFFLINE */}
+          <Badge variant="dot"
+            color="error" showZero badgeContent="" overlap="circular"></Badge>
         </div>
         <div className="details">
           <p className="name">{friend.name}</p>
@@ -37,12 +44,12 @@ export default function Chat_sidebar({ friends }) {
       </div>
 
       {!selectedFriend && <div className="default">
-        <ChatIcon style={{fontSize: 200}}/>
+        <ChatIcon style={{ fontSize: 200 }} />
       </div>}
 
       {selectedFriend && (
         <div className="chat-main">
-          
+
           <Chat_header selected={selectedFriend}></Chat_header>
           <Chat_area selected={selectedFriend}></Chat_area>
           <Chat_input></Chat_input>
