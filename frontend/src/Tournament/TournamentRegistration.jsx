519c86caab8e93bb1d1bdbf5fc9f55148bf878eb
USM4 <oussamaredoine06@gmail.com>
2024-11-27 12:58:18 +0100

    have to check the players of the tournament before submiting to the tournament

--- frontend/src/Tournament/TournamentRegistration.jsx
+++ frontend/src/Tournament/TournamentRegistration.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import RenderPlayers from './RenderPlayers.jsx';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 
 const TournamentRegistration = () => {
-  const [playerName, setPlayerName] = useState("");
+
   const navigate = useNavigate();
 
   return (
@@ -13,64 +15,35 @@ const TournamentRegistration = () => {
         <div className="registration-title">
           <div className="return-icon">
             <button
-              onClick={() => {
-                navigate("/tournament/options");
-              }}
-            >
+              onClick={() => { navigate("/tournament/options"); }}>
               <ArrowBackIcon fontSize="large" />
             </button>
           </div>
           <div className="title-trnmt">
             <p>Ping-Pong Tournament Registration </p>
+            
           </div>
         </div>
         <div className="tournament-players-components">
-          <div className="tournament-players">
-            {/* <h3>Player Name:</h3>
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  />
-                  <input
-                    type="text"
-                    value={playerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                  /> */}
-          </div>
-          <div className="tournament-rules-component">
-                    {/* <h3>Tournament Rules:</h3>
-                    <ul>
-                      <li>Players must be available at match time.</li>
-                      <li>Players should be more than 3 (3la hasab)</li>
-                      <li>10 minutes break between each round.</li>
-                    </ul> */}
-          </div>
+          <RenderPlayers/>
+            <div className="tournament-rules">
+                <WarningAmberIcon />
+                <h3>Tournament Rules:</h3>
+                <ul>
+                  <li>Players must be available at match time.</li>
+                  <li>Players should be more than 3 (3la hasab)</li>
+                  <li>10 minutes break between each round.</li>
+                </ul>
+                <div className="next-to-trnmt">
+                  <button
+                    onClick={() => {
+                      navigate("/tournament/options/play-tournament");
+                    }}
+                  >
+                    Next
+                  </button>
+                </div>
+            </div>
         </div>
       </div>
     </div>
