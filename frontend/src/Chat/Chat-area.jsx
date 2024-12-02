1595350ea47b81a16ba851b972cf90948606aac1
yahiarrh <yahiarrh@gmail.com>
2024-12-02 15:33:33 +0100

    fixing block

--- frontend/src/Chat/Chat-area.jsx
+++ frontend/src/Chat/Chat-area.jsx
@@ -5,27 +5,42 @@ import BlockIcon from '@mui/icons-material/Block';
 
 export default function Chat_area({ selected }) {
 
-  const [message, setMessage] = useState([]);
+  const [message, setMessage] = useState({});
+  const [chatroom, setChatroom] = useState();
   const socket = useContext(ChatSocketContext);
 
   useEffect(() => {
-    setMessage([])
-
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data);
-      if (data.receiver == selected.id || data.sender == data.receiver || data.sender == selected.id)
-        console.log("message received")
-        if (data.message)
-          setMessage((prevMessages) => [...prevMessages, data]);
-    };
-    return () => {
-      socket.onmessage = null;
+      const { chat_room, message, message_id } = data;
+
+      setChatroom(chat_room);
+      if (message){
+
+        setMessage((prevMessage) => {
+          const chatMessage = prevMessage[chat_room] || [];
+          const messageExists = chatMessage.some((msg) => msg.message_id === message_id);
+          
+          
+          if(!messageExists) {
+            return {
+              ...prevMessage,
+              [chat_room]: [...chatMessage, data],
+            };
+          }
+          return prevMessage;
+        });
+      }
     };
   }, [socket, selected])
-  console.log(selected.is_blocked)
 
 
-  const message_history = message.map((msg, index) => (
+  const chatroomMessages = message[chatroom] || [];
+  // console.log(chatroomMessages)
+  // console.log(chatroom)
+  // console.log(selected)
+
+  const message_history = chatroomMessages.map((msg, index) => (
     <div key={index}>
       {msg.receiver != selected.id
         ? (<div className="message-sent">{msg.message}</div>)
@@ -37,9 +52,9 @@ export default function Chat_area({ selected }) {
 
   return (
     <div className="chat-area">
-      
-        {!selected.is_blocked ? <div className="message-wrap">{message_history}</div> : <div className="blocked">THIS USER IS BLOCKED <BlockIcon fontSize='large' /></div>}
-      
+
+      {!selected.is_blocked ? <div className="message-wrap">{message_history}</div> : <div className="blocked">THIS USER IS BLOCKED <BlockIcon fontSize='large' /></div>}
+
     </div>
   );
 }
