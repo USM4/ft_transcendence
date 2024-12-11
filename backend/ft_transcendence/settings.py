940b6b67b07bc7aa02a9abdf9ee84c14d22d302f
USM4 <oussamaredoine06@gmail.com>
2024-12-11 02:53:53 +0100

    Welcome Nginx should be replaced by the project files

--- backend/ft_transcendence/settings.py
+++ backend/ft_transcendence/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-w@)mu=t95zxg_&mu1l13+4d$rd9$4g0i65^1y8b&woqlvlk7st
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
 
@@ -157,6 +157,7 @@ CHANNEL_LAYERS = {
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost',
 ]
 
 # Internationalization
