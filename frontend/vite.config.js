8d6cd3014a78f97d50d0c020cddd69ee154b0a3f
USM4 <oussamaredoine06@gmail.com>
2024-12-11 01:11:24 +0100

    docker compose for the backend and the frontend works properly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: false,  // Disable source maps for production builds
-  }
+  },
+  server: {
+    host: '0.0.0.0',  // Make the app accessible from outside the container
+    port: 5173,       // You can specify the port your app runs on
+  },
 })
