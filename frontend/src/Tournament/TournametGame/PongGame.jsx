afab784fe99c647def8cd6e523a93a4129cdd508
yahiarrh <yahiarrh@gmail.com>
2024-12-17 20:03:56 +0100

    updated tournament with new game

--- frontend/src/Tournament/TournametGame/PongGame.jsx
+++ frontend/src/Tournament/TournametGame/PongGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback,useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Canvas from './Canvas';
 import Ball from './Ball';
@@ -7,19 +7,37 @@ import Racket from './Racket';
 import player1Image from '/pensioner.png';
 import player2Image from '/pensioner.png';
 
+
 const PongGame = () => {
   const navigate = useNavigate();
   const matche = JSON.parse(localStorage.getItem('matche')) || {};
-  const [ball, setBall] = useState({
-    x: 500, y: 250, velocityX: 4, velocityY: 4, radius: 10, color: '#fff'
+  const canvasRef = useRef(null);
+  const keys = {};
+  const ballRef = useRef({
+    x: 500,
+    y: 250,
+    velocityX: 2,
+    velocityY: 2,
+    radius: 10,
+    color: "yellow",
   });
 
-  const [leftRacket, setLeftRacket] = useState({
-    x: 10, y: 200, width: 10, height: 100, color: '#000000', velocity: 20,
+  const leftRacket = useRef({
+    x: 10,
+    y: 200,
+    width: 10,
+    height: 100,
+    color: "blue",
+    velocity: 13,
   });
 
-  const [rightRacket, setRightRacket] = useState({
-    x: 980, y: 200, width: 10, height: 100, color: '#000000', velocity: 20,
+  const rightRacket = useRef({
+    x: 980,
+    y: 200,
+    width: 10,
+    height: 100,
+    color: "red",
+    velocity: 13,
   });
 
   const [keysPressed, setKeysPressed] = useState({
@@ -33,232 +51,288 @@ const PongGame = () => {
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
-    setBall({ x: 500, y: 250, velocityX: 4, velocityY: 4, radius: 10, color: '#fff' });
-    setLeftRacket((prev) => ({ ...prev, y: 200 }));
-    setRightRacket((prev) => ({ ...prev, y: 200 }));
+    ballRef.current = {
+      x: 500,
+      y: 250,
+      velocityX: 4,
+      velocityY: 4,
+      radius: 10,
+      color: "yellow",
+    };
+    // setLeftRacket((prev) => ({ ...prev, y: 200 }));
+    // setRightRacket((prev) => ({ ...prev, y: 200 }));
     setScores({ leftPlayer: 0, rightPlayer: 0 });
+    setWinner(null);
+    canvasRef.current = null;
   };
 
   const moveAIRacket = () => {
-    setRightRacket((prev) => {
-      const direction = ball.y > prev.y + prev.height / 2 ? 1 : -1;
-      let newY = prev.y + direction * prev.velocity;
-      newY = Math.max(0, Math.min(newY, 500 - prev.height));
-      return { ...prev, y: newY };
-    });
+    // setRightRacket((prev) => {
+    if (ballRef.current.velocityX < 0) return;
+    const direction =
+      ballRef.current.y > rightRacket.current.y + rightRacket.current.height / 2
+        ? 1
+        : -1;
+    let newY = rightRacket.current.y + direction * rightRacket.current.velocity;
+    newY = Math.max(0, Math.min(newY, 500 - rightRacket.current.height));
+    rightRacket.current = { ...rightRacket.current, y: newY };
+    // });
   };
 
   const resetPositions = () => {
-    setLeftRacket((prev) => ({
-      ...prev,
+    // setLeftRacket((prev) => ({
+    leftRacket.current = {
+      ...leftRacket.current,
       y: 200,
-    }));
-    setRightRacket((prev) => ({
-      ...prev,
+    };
+    // }));
+
+    rightRacket.current = {
+      ...rightRacket.current,
       y: 200,
-    }));
+    };
   };
 
   const updateBallPosition = useCallback(() => {
-    setBall((prevBall) => {
-      let { x, y, velocityX, velocityY } = prevBall;
-      x += velocityX;
-      y += velocityY;
-  
-      if (y - ball.radius <= 0 || y + ball.radius >= 500) {
-        velocityY = -velocityY;
-      }
-  
-      // Left Racket Collision
-      if (
-        x - ball.radius <= leftRacket.x + leftRacket.width &&
-        y >= leftRacket.y &&
-        y <= leftRacket.y + leftRacket.height
-      ) {
-        velocityX = Math.abs(velocityX);
-        x = leftRacket.x + leftRacket.width + ball.radius;
-      }
-  
-      // Right Racket Collision
-      if (
-        x + ball.radius >= rightRacket.x &&
-        y >= rightRacket.y &&
-        y <= rightRacket.y + rightRacket.height
-      ) {
-        velocityX = -Math.abs(velocityX);
-        x = rightRacket.x - ball.radius;
-      }
-  
-      let updatedScores = { ...scores };
-      if (x - ball.radius <= 0) {
-        // Right player scores
-        updatedScores.rightPlayer += 1;
-        setScores(updatedScores); // Update score
-  
-        // Check if anyone won
-        if (updatedScores.rightPlayer === 2) {
-          matche.winner = matche.Player2;
-          matche.score1 = updatedScores.leftPlayer;
-          matche.score2 = updatedScores.rightPlayer;
-          setWinner(matche.Player2);
-          localStorage.setItem('matchePlayed', JSON.stringify(matche));
-          localStorage.removeItem('matche');
+    // setBall((prevBall) => {
+    let { x, y, velocityX, velocityY } = ballRef.current;
+    x += velocityX + Math.random() * 0.5;
+    y += velocityY + Math.random() * 0.5;
+
+    if (y - ballRef.current.radius <= 0) {
+      y = ballRef.current.radius;
+      velocityY = -velocityY;
+    }
+    if (y + ballRef.current.radius >= 500) {
+      y = 500 - ballRef.current.radius;
+      velocityY = -velocityY;
+    }
+
+    if (
+      x - ballRef.current.radius <=
+        leftRacket.current.x + leftRacket.current.width &&
+      y >= leftRacket.current.y &&
+      y <= leftRacket.current.y + leftRacket.current.height
+    ) {
+      velocityX = Math.abs(velocityX);
+      x =
+        leftRacket.current.x +
+        leftRacket.current.width +
+        ballRef.current.radius;
+    }
+
+    if (
+      x + ballRef.current.radius >= rightRacket.current.x &&
+      y >= rightRacket.current.y &&
+      y <= rightRacket.current.y + rightRacket.current.height
+    ) {
+      velocityX = -Math.abs(velocityX);
+      x = rightRacket.current.x - ballRef.current.radius;
+    }
+    ballRef.current = {
+      ...ballRef.current,
+      x: x,
+      y: y,
+      velocityX: velocityX,
+      velocityY: velocityY,
+    };
+
+    if (x - ballRef.current.radius <= 0) {
+      setScores((prevScores) => {
+        if (prevScores.rightPlayer + 1 === 2) {
+          setWinner("Player 1");
         }
-  
-        resetPositions();
-        return { ...prevBall, x: 500, y: 250, velocityX: 4, velocityY: 4 };
-      } else if (x + ball.radius >= 1000) {
-        // Left player scores
-        updatedScores.leftPlayer += 1;
-        setScores(updatedScores); // Update score
-  
-        // Check if anyone won
-        if (updatedScores.leftPlayer === 2) {
-          matche.winner = matche.Player1;
-          matche.score1 = updatedScores.leftPlayer;
-          matche.score2 = updatedScores.rightPlayer;
-          setWinner(matche.Player1);
-          localStorage.setItem('matchePlayed', JSON.stringify(matche));
-          localStorage.removeItem('matche');
+        return {
+          ...prevScores,
+          rightPlayer: prevScores.rightPlayer + 1,
+        };
+      });
+      resetPositions();
+      ballRef.current = {
+        ...ballRef.current,
+        x: 500,
+        y: 250,
+      };
+    } else if (x + ballRef.current.radius >= 1000) {
+      setScores((prevScores) => {
+        if (prevScores.leftPlayer + 1 === 2) {
+          setWinner("Player 2");
         }
-  
-        resetPositions();
-        return { ...prevBall, x: 500, y: 250, velocityX: -4, velocityY: 4 };
-      }
-  
-      // Return ball position
-      return { ...prevBall, x, y, velocityX, velocityY };
-    });
-  }, [leftRacket, rightRacket, scores]);
-  
+        return {
+          ...prevScores,
+          leftPlayer: prevScores.leftPlayer + 1,
+        };
+      });
+      resetPositions();
+      ballRef.current = {
+        ...ballRef.current,
+        x: 500,
+        y: 250,
+      };
+    }
+    // setScores(updatedScores);
+  });
 
-  
+  // useEffect(() => {
+  //   window.addEventListener('keydown', handleKeyDown);
+  //   window.addEventListener('keyup', handleKeyUp);
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyDown);
+  //     window.removeEventListener('keyup', handleKeyUp);
+  //   };
+  // }, []);
 
-  const moveLeftRacket = (direction) => {
-    setLeftRacket((prev) => {
-      let newY = prev.y + direction * prev.velocity;
-      newY = Math.max(0, Math.min(newY, 500 - prev.height));
-      return { ...prev, y: newY };
-    });
-  };
+  useEffect(() => {
+    const moveLeftRacket = (direction) => {
+      // setLeftRacket((prev) => {
+      let newY = leftRacket.current.y + direction * leftRacket.current.velocity;
+      newY = Math.max(0, Math.min(newY, 500 - leftRacket.current.height));
+      leftRacket.current = { ...leftRacket.current, y: newY };
+      // });
+    };
 
-  const moveRightRacket = (direction) => {
-    setRightRacket((prev) => {
-      let newY = prev.y + direction * prev.velocity;
-      newY = Math.max(0, Math.min(newY, 500 - prev.height));
-      return { ...prev, y: newY };
-    });
-  };
-
-  const handleKeyDown = (e) => {
-    setKeysPressed((prev) => {
-      const updatedKeys = { ...prev, [e.key]: true };
-
-      if (updatedKeys.w) {
+    const moveRightRacket = (direction) => {
+      // setRightRacket((prev) => {
+      let newY =
+        rightRacket.current.y + direction * rightRacket.current.velocity;
+      newY = Math.max(0, Math.min(newY, 500 - rightRacket.current.height));
+      rightRacket.current = { ...rightRacket.current, y: newY };
+      // });
+    };
+    const handleKeyDown = (e) => {
+      keys[e.key] = true; // Set the key state to true when pressed
+      if (keys["w"] || keys["W"]) {
         moveLeftRacket(-1);
       }
-      if (updatedKeys.s) {
+      if (keys["s"] || keys["S"]) {
         moveLeftRacket(1);
       }
 
-      if (updatedKeys.o) {
+      if (keys["ArrowUp"]) {
         moveRightRacket(-1);
       }
-      if (updatedKeys.l) {
+      if (keys["ArrowDown"]) {
         moveRightRacket(1);
       }
-
-      return updatedKeys;
-    });
-  };
-
-  const handleKeyUp = (e) => {
-    setKeysPressed((prev) => {
-      const updatedKeys = { ...prev, [e.key]: false };
-      return updatedKeys;
-    });
-  };
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('keyup', handleKeyUp);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keyup', handleKeyUp);
+      
     };
-  }, []);
 
-  useEffect(() => {
-    const gameInterval = setInterval(() => {
-      if (!matche.winner) {
-        updateBallPosition();
+    window.addEventListener("keydown", handleKeyDown);
+    const handleKeyUp = (e) => {
+      keys[e.key] = false;
+    };
+    window.addEventListener("keyup", handleKeyUp);
+
+    const animate = () => {
+      if (canvasRef.current === null) {
+        console.log("Canvas is null");
+        return;
       }
-    }, 16);
-  
-    return () => clearInterval(gameInterval);
-  }, [updateBallPosition, moveAIRacket, matche.winner]);
-
-  const draw = useCallback(
-    (context) => {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
       context.clearRect(0, 0, 1000, 500);
-      context.fillStyle = '#326da4';
+      context.fillStyle = "#000000";
       context.fillRect(0, 0, 1000, 500);
 
+      updateBallPosition();
       context.beginPath();
-      context.fillStyle = ball.color;
-      context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+      context.fillStyle = ballRef.current.color;
+      context.arc(
+        ballRef.current.x,
+        ballRef.current.y,
+        ballRef.current.radius,
+        0,
+        Math.PI * 2
+      );
       context.fill();
 
-      context.fillStyle = leftRacket.color;
-      context.fillRect(leftRacket.x, leftRacket.y, leftRacket.width, leftRacket.height);
+      context.fillStyle = leftRacket.current.color;
+      context.fillRect(
+        leftRacket.current.x,
+        leftRacket.current.y,
+        leftRacket.current.width,
+        leftRacket.current.height
+      );
 
-      context.fillStyle = rightRacket.color;
-      context.fillRect(rightRacket.x, rightRacket.y, rightRacket.width, rightRacket.height);
-      if (matche.winner) return;
-    },
-    [ball, leftRacket, rightRacket]
-  );
+      context.fillStyle = rightRacket.current.color;
+      context.fillRect(
+        rightRacket.current.x,
+        rightRacket.current.y,
+        rightRacket.current.width,
+        rightRacket.current.height
+      );
+      requestAnimationFrame(animate);
+      // if (winner) return;
+    };
+    animate();
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [canvasRef.current]);
 
   useEffect(() => {
-    const matcheP = JSON.parse(localStorage.getItem('matchePlayed')) || {};
-    if (matcheP.winner) {
+    if (winner) {
+      if (winner === "Player 1") {        
+        matche.winner = matche.Player1;
+      } else {
+        matche.winner = matche.Player2;
+      }
+      matche.score1 = scores.leftPlayer;
+      matche.score2 = scores.rightPlayer;
+      localStorage.setItem('matchePlayed', JSON.stringify(matche));
+      localStorage.removeItem('matche');
       navigate("/tournament/options/play-tournament");
+      console.log("Winner is", winner);
     }
   }, [winner, navigate]);
 
   return (
-    <div className='Game-render'>
+    <div className="Game-render">
       <div className="player-profiles">
-        <div>
-        <img src={player1Image}></img>
-          <h3>{matche.Player1}</h3>
-          <p>Score: {scores.leftPlayer}</p>
+        <div className="player-card">
+          <div className="player-name">
+            <h3>{matche.Player1}</h3>
+            <span className="status-dot active"></span>
+          </div>
+          <div className="score-container">
+            <span className="score">{scores.leftPlayer}</span>
+          </div>
+          <div className="player-avatar">
+            <img src={player1Image} alt="Player 1" />
+            <div className="glow-effect"></div>
+          </div>
         </div>
-        <div>
-          <img src={player2Image}></img>
-          <h3>{matche.Player2}</h3>
-          <p>Score: {scores.rightPlayer}</p>
+        
+        <div className="vs-container">
+          <span className="vs-text">VS</span>
+        </div>
+        
+        <div className="player-card">
+          <div className="player-name">
+            <h3>{matche.Player2}</h3>
+            <span className="status-dot active"></span>
+          </div>
+          <div className="score-container">
+            <span className="score">{scores.rightPlayer}</span>
+          </div>
+          <div className="player-avatar">
+            <img src={player2Image} alt="Player 2" />
+            <div className="glow-effect"></div>
+          </div>
         </div>
       </div>
-      <div className='canvas-container'>
-        <Canvas draw={draw} width={1000} height={500} />
+      <div className="canvas-container">
+        <canvas
+          className="canvas-game"
+          ref={canvasRef}
+          width={1000}
+          height={500}
+        />
+        <div className="match-timer">00:00</div>
       </div>
-      <Ball x={ball.x} y={ball.y} radius={ball.radius} color={ball.color} updatePosition={updateBallPosition} />
-      <Racket x={leftRacket.x} y={leftRacket.y} width={leftRacket.width} height={leftRacket.height} color={leftRacket.color} upKey="w" downKey="s" onMove={moveLeftRacket} />
-        <Racket x={rightRacket.x} y={rightRacket.y} width={rightRacket.width} height={rightRacket.height} color={rightRacket.color} upKey="o" downKey="l" onMove={moveRightRacket} />
-      <h3>
-        {/* <img src='../img/W-key.png' alt="W key" className="key-img" />  */}
-        {/* <img src='../img/S-key.png' alt="S key" className="key-img" />  */}
-        Click W to go up and S to go down
-      </h3>
-        <h3>
-          {/* <img src='../img/O-key.png' alt="O key" className="key-img" />  */}
-          {/* <img src='../img/L-key.png' alt="L key" className="key-img" />  */}
-          Click O to go up and L to go down
-        </h3>
     </div>
   );
 };
 
 export default PongGame;
-
