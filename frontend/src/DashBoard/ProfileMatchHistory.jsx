2e3b8ac56c43d121cb828b0d66e714524d12fd42
yahiarrh <yahiarrh@gmail.com>
2024-12-26 23:25:08 +0100

    fixing profile charts

--- frontend/src/DashBoard/ProfileMatchHistory.jsx
+++ frontend/src/DashBoard/ProfileMatchHistory.jsx
@@ -12,40 +12,47 @@ function ProfileMatchHistory() {
     const { user } = useContext(UserDataContext);
 
 
-    return(
+    return (
         <div className="profile-match-history-item">
             {user?.matchePlayed.length === 0 && <h1 className="data-chart-h1">No matches played yet</h1>}
-                {user?.matchePlayed.map((match) => {
-                    const player1 = match[0]['player1'];
-                    const player2 = match[0]['player2'];
-                    return (
-                        <div key={match[0].id} className="match-history-item">
-                            <div className="match-history-info">
-                                <img
-                                    src={player1.avatar || anonyme}
-                                    alt={`${player1.username}'s avatar`}
-                                    className="img-player"
-                                />
-                                {player1.username}
-                            </div>
-                            <div className="match-history-score">
-                                {`${match[0]['score_player1']} - ${match[0]['score_player2']}`}
-                            </div>
-                            <div className="match-history-info">
-                                {player2.username}
+            {user?.matchePlayed.map((match) => {
+                const player1 = match[0]['player1'];
+                const player2 = match[0]['player2'];
+                return (
+                    <div key={match[0].id} className="profile-matches">
+                        <div className="profile-match-history-info">
+                            <img
+                                src={player1.avatar || anonyme}
+                                alt={`${player1.username}'s avatar`}
+                                className="img-player"
+                            />
+                            {player1.username}
+                        </div>
+                        <div className="match-history-score">
+                            {`${match[0]['score_player1']} - ${match[0]['score_player2']}`}
+                        </div>
+                        <div className="profile-match-history-info">
+                            {player2.username}
+                            <div className="details-icon">
+
                                 <img
                                     src={player2.avatar || anonyme}
                                     alt={`${player2.username}'s avatar`}
-                                    className="img-player2"
                                 />
-                            <button ><DetailsIcon/></button>
+                                <button ><DetailsIcon
+                                    onClick={() => {
+                                        setMatchDetails(match[0]);
+                                        setOpen(true);
+                                    }}
+                                /></button>
                             </div>
                         </div>
-                    );
-                })}
+                    </div>
+                );
+            })}
         </div>
     );
-    
+
 };
 
 export default ProfileMatchHistory;
