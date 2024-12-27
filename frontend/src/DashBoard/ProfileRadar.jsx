78806e173e4928a0292998d16317122516346e28
yahiarrh <yahiarrh@gmail.com>
2024-12-27 18:31:46 +0100

    Profilr radar khaso i3adat nadar

--- frontend/src/DashBoard/ProfileRadar.jsx
+++ frontend/src/DashBoard/ProfileRadar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useContext} from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Radar } from 'react-chartjs-2';
 import {
@@ -11,25 +11,38 @@ import {
   Legend,
 } from 'chart.js';
 import { colors } from "@mui/material";
+import { UserDataContext } from "./UserDataContext.jsx";
+import { FriendDataContext } from "./FriendDataContext.jsx";
 ChartJS.register( RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
 
-function ProfileRadar() {
+function ProfileRadar({ profile,is_user}) {
+    const {friends, setFriends} = useContext(FriendDataContext);
+    
+    let { user } = useContext(UserDataContext);
+    if (is_user)
+        friends.map((friend) => { friend.username === profile.username ? user = friend : null; });
+
+//     Total Games Played
+// Games Won
+// Win Rate (calculated as games won / total games played)
+// Average Score (average score per game)
+// Total XP Gained
+
 
     const data = {
         labels: [
-            'Offensive Skills',
-            'Defensive Skills',
-            'Speed',
-            'Shot Precision',
-            'Consistency',
-            'Strategy',
-            'Tactical Awareness',
+            'Total Games Played',
+            'Games Won',
+            'Win Rate',
+            'Average Score',
+            'Total XP Gained',
+            
         ],
         datasets: 
         [
             {
                 label: "Player Skills",
-                data: [80, 80, 100, 80, 100, 70, 100],
+                data: [user.matchePlayed.length, user?.matcheWon, user?.win_rate, user?.average_xp, user?.total_xp],
                 fill: true,
                 backgroundColor: 'rgba(255, 111, 97, 0.5)',
                 borderColor: 'rgba(255, 111, 97, 1)',
@@ -48,11 +61,11 @@ function ProfileRadar() {
         }
     },
     scales: {
+        
         r: {
-        suggestedMin: 50,
-        suggestedMax: 100,
+            // beginAtZero: true,
             ticks: {
-                stepSize: 10,
+                stepSize: 1,
                 color: 'black',
                 backdropColor: 'rgba(0, 0, 0, 0)',
             }
